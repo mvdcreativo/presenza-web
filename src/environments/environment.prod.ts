922ffd8094a22bad93574c5aca61b5f2ccd95e8b
unknown <mvdcreativo@gmail.com>
2020-08-06 10:47:36 -0300

    environment picaworks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API:"http://api.presenza.picaworks.com/api/",
+  storageAPI:"http://api.presenza.picaworks.com/storage/",
+  API_KEY_GM : "AIzaSyDYkJQ4CJrY3YrvR-1fcn36K4-iuyZsiGI",
+  urlAssets : "assets/"
 };
