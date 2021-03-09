183e3ae6dbe42158c9f54796bc1e328906c8f08e
picaporte-design <mvdcreativo@gmail.com>
2021-03-09 02:04:36 -0300

    update rxjs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API:"http://api.presenza.picaworks.com/api/",
   storageAPI:"http://api.presenza.picaworks.com/storage/",
-  API_KEY_GM : "AIzaSyDYkJQ4CJrY3YrvR-1fcn36K4-iuyZsiGI",
+  API_KEY_GM : "AIzaSyBgkTr7pr2hwsa4Gkk499GLfcm01j0CsvQ",
   urlAssets : "assets/"
 };
