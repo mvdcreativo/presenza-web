3784377c146532b08d22deea9bf53d9d95f6297d
picaporte-design <mvdcreativo@gmail.com>
2021-03-29 02:05:50 -0300

    correccion error compilacion

--- src/app/theme/directives/directives.module.ts
+++ src/app/theme/directives/directives.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnlyNumberDirective } from './only-number.directive';
-import { StikyElementDirective } from './stiky-element.directive';
 
 @NgModule({
   declarations: [
     OnlyNumberDirective,
-    StikyElementDirective
+    
   ],
   exports: [
     OnlyNumberDirective,
-    StikyElementDirective
+    
   ],
   imports: [
     CommonModule
